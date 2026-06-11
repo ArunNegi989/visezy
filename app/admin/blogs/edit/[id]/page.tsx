4fc9f6a7a8b70bfae2e2caa53735a66d76529ed9
harshrajsinhvaghela7586 <hemantsinhvaghela3093@gmail.com>
2026-06-11 17:00:38 +0530

    Fix Netlify Error

---
+++ app/admin/blogs/edit/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function EditBlogPage() {
+  return (
+    <div style={{ padding: "24px" }}>
+      <h1>Edit Blog Page</h1>
+      <p>This page is under development.</p>
+    </div>
+  );
+}
